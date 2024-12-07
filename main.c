9fca437413e1235ad08f8db458b14a89c4b28c1d
ChahirSaid <80861946+ChahirSaid@users.noreply.github.com>
2024-12-07 14:50:22 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -1,7 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: schahir <schahir@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/07 14:36:31 by schahir           #+#    #+#             */
+/*   Updated: 2024/12/07 14:49:05 by schahir          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 #include <stdio.h>
-
-int	main(void)
+/*int main()
 {
-	ft_printf("%5d", ft_printf(NULL));
+	 int a =58;
+	//scanf("%d", &a);
+	int x = printf("|%#.5x|", a);
+	printf("\n");
+	int y = ft_printf("|%#.5x|", a);
+	printf("\n Expected %d to deeply equal to %d", x, y);
+}
+
+int main()
+{
+	 int a = -58;
+	//scanf("%d", &a);
+	int x = printf("|%10.5d|", a);
+	printf("\n");
+	int y = ft_printf("|%10.5d|", a);
+	printf("\n Expected %d to deeply equal to %d", x, y);
+}
+
+int main()
+{
+	 int a;
+	scanf("%d", &a);
+	int x = printf("|%+10.7d|", a);
+	printf("\n");
+	int y = ft_printf("|%+10.7d|", a);
+	printf("\n Expected %d to deeply equal to %d", x, y);
+}*/
+int main()
+{
+	 int a;
+	scanf("%d", &a);
+	int x = printf("|%+10.7d|", a);
+	printf("\n");
+	int y = ft_printf("|%+10.7d|", a);
+	printf("\n Expected %d to deeply equal to %d", x, y);
 }
